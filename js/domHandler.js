089ecf38ba67c3050d46cf6a83e7694a9ac758f1
Tyler Dixon <onyxqk@gmail.com>
2019-09-06 18:48:55 -0400

    added medieval

--- js/domHandler.js
+++ js/domHandler.js
@@ -2,24 +2,28 @@ function ioHandler() {
   var content = document.getElementById('content');
   var selectedRuneSet = document.getElementById('runeSets').options[runeSets.selectedIndex].value;
   var input = document.getElementById('input').value;
-  switch(selectedRuneSet) {
+  switch (selectedRuneSet) {
     case "elderFuthark":
       runeSet = elderFuthark;
       break;
     case "futhorc":
-        runeSet = futhorc;
+      runeSet = futhorc;
       break;
     case "longBranch":
-        runeSet = longBranch;
-        break;
+      runeSet = longBranch;
+      break;
     case "shortTwig":
-        runeSet = shortTwig;
+      runeSet = shortTwig;
+      break;
+    case "medieval":
+      runeSet = medieval;
+      break;
   }
   content.textContent = convert(input, runeSet);
 }
 
 function enterHandler(e) {
-  if (e.which == 13) {
+  if (e.which === 13) {
     ioHandler();
   }
 }
@@ -27,4 +31,5 @@ function enterHandler(e) {
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('button').addEventListener('click', ioHandler);
   document.querySelector('input').addEventListener('keypress', enterHandler);
+  document.querySelector('select').addEventListener('keypress', enterHandler);
 });
