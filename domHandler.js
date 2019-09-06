49871ed04913e3809f0de4e2fe889f342c631c82
Tyler Dixon <onyxqk@gmail.com>
2019-09-05 20:34:03 -0400

    enhanced input field

--- domHandler.js
+++ domHandler.js
@@ -6,6 +6,14 @@ function clickHandler(e) {
     setTimeout(manageInput, 50);
   }
 
+
+function enterHandler(e) {
+    if (e.which == 13 || event.keyCode == 13) {
+        manageInput();
+    }
+  }
+
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('button').addEventListener('click', clickHandler);
+    document.querySelector('input').addEventListener('keypress', enterHandler);
   });
